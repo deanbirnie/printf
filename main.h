8a3f493d35763bf9b594080a0608f4b330154431
MichaelRobsonZA <michaelrobsonza@gmail.com>
2023-03-26 02:12:24 -0700

    Update header file with function prototypes and struct definition

--- main.h
+++ main.h
@@ -1,11 +1,28 @@
-#ifndef MAIN_H_
-#define MAIN_H_
+#ifndef MAIN_H
+#define MAIN_H
 
 #include <stdlib.h>
 #include <stdarg.h>
 
+/* This function writes a character to the standard output and returns
+ * the number of characters written.
+ */
 int _putchar(char c);
 
+/* This function prints a formatted string to the standard output and
+ * returns the number of characters printed.
+ */
 int _printf(const char *format, ...);
 
+/* This function prints a string argument to the standard output and
+ * returns the number of characters printed.
+ */
+int print_string(va_list arg_list);
+
+/* This function prints an integer argument to the standard output and
+ * returns the number of characters printed.
+ */
+int print_integer(va_list arg_list);
+
 #endif
+
