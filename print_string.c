df5821a885102c26563695aed97c4a9a851a8781
MichaelRobsonZA <michaelrobsonza@gmail.com>
2023-03-28 02:45:25 -0700

    Refactor print_string function Simplified the logic of the print_string function, added a comment explaining the purpose of the counter variable, and added a default string to print if the input string is null.

--- print_string.c
+++ print_string.c
@@ -1,34 +1,26 @@
 #include "main.h"
 
-#include <stdio.h>
-
-/*
- * This function, named print_string, is used to print a string. It takes in a
- * char pointer.
- *
- * @str: the string to print
- *
- * The function returns an integer value which indicates the number of
- * characters printed.
+/**
+ * print_string - Prints a string
+ * @list: list of arguments
+ * Return: Will return the amount of characters printed.
  */
-int print_string(char *str)
+int print_string(va_list list)
 {
-    /* Declare a counter variable i */
-    int i;
+    int i;          /* Declare a variable to keep track of the number of characters printed */
+    char *str;      /* Declare a variable to hold the next string argument from the list */
 
-    /* If the string is null, then print a default string */
+    /* Get the next string argument from the list */
+    str = va_arg(list, char *);
+
+    /* If the string is NULL, print (null) */
     if (str == NULL)
-    {
         str = "(null)";
-    }
 
-    /* Loop through each character in the string and print it to the console */
+    /* Loop through the string and print each character */
     for (i = 0; str[i] != '\0'; i++)
-    {
         _putchar(str[i]);
-    }
 
     /* Return the number of characters printed */
     return (i);
 }
-
