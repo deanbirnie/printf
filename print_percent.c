097ed3d0f0c9f5e07383eaf47c251c5a0292dce2
deanbirnie <deanicdl@gmail.com>
2023-03-28 03:12:27 -0700

    Created print_percent which prints the % symbol to console

---
+++ print_percent.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * print_percent - prints the % sign to console
+ *
+ * Return: returns 1, the number of characters printed to the console
+ */
+int print_percent(void)
+{
+	_putchar('%');
+
+	return (1);
+}
