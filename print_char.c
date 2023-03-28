1a2ea853f94a064f51c8b726ba8ad458963482e1
deanbirnie <deanicdl@gmail.com>
2023-03-28 03:11:47 -0700

    Created print_char which  prints a given character to the console

---
+++ print_char.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * print_char - prints characters as a helper for printf fucntion
+ *
+ * @c: character to print to console
+ *
+ * Return: returns 1 for the number of characters printed
+ */
+int print_char(char c)
+{
+	_putchar(c);
+
+	return (1);
+}
