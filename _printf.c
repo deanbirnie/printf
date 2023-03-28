daca5f20fefa1cb6ae2397ad4791e02108e7ff8c
MichaelRobsonZA <michaelrobsonza@gmail.com>
2023-03-28 02:45:30 -0700

    Refactor _printf function to use a new conversion specifier list and add comments for clarity

--- _printf.c
+++ _printf.c
@@ -2,41 +2,37 @@
 #include <stdarg.h>
 
 /**
- * _printf - Prints a formatted string
- * @format: The format string
- *
- * Return: The number of characters printed (excluding the null byte used
- *         to end output to strings)
+ * _printf - Receives the main string and all the necessary parameters to
+ * print a formatted string
+ * @format: A string containing all the desired characters
+ * Return: A total count of the characters printed
  */
 int _printf(const char *format, ...)
 {
-    int printed_chars;
-    va_list arg_list;
+    int printed_chars = 0;
 
-    /* Define an array of structures, containing format specifiers and their
-     * corresponding functions to be called by format_reciever function
-     */
+    /* The f_list array contains a list of conversion specifiers and their corresponding function pointers. */
     conver_t f_list[] = {
-        {"s", print_string},
-        {"d", print_integer},
-        {"i", print_integer},
-        {NULL, NULL} /* End of the array */
+        {"c", print_char},    /* 'c' conversion specifier for printing a single character */
+        {"s", print_string},  /* 's' conversion specifier for printing a string of characters */
+        {"%", print_percent}, /* '%' conversion specifier for printing a '%' character */
+        {"d", print_int},     /* 'd' conversion specifier for printing a decimal (base 10) integer */
+        {"i", print_int},     /* 'i' conversion specifier for printing a signed decimal (base 10) integer */
     };
 
-    /* Check if format is NULL */
+    va_list arg_list; /* The va_list type is used for traversing the variable argument list passed to a function */
+
+    /* Check if format string is NULL */
     if (format == NULL)
         return (-1);
 
-    /* Initialize arg_list to retrieve variable number of arguments */
+    /* Initialize arg_list to the first optional argument after the format string */
     va_start(arg_list, format);
 
-    /* Call the format_reciever function, which uses the format string and
-     * the array of format specifiers and their corresponding functions to
-     * print the formatted string
-     */
-    printed_chars = format_reciever(format, f_list, arg_list);
+    /* Calls format_receiver() with format string, conversion specifier list, and arg_list as arguments */
+    printed_chars = format_receiver(format, f_list, arg_list);
 
-    /* Clean up the argument list */
+    /* Clean up arg_list */
     va_end(arg_list);
 
     /* Return the total number of characters printed */
